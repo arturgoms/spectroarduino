9ed7b2c921445888129cbf2c89b843e624603e67
Artur Gomes <flashballclube@gmail.com>
2016-03-04 19:56:51 -0300

    Added files via upload

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# arduino
+Spectro Watch DYI v1.0
+=========
+
+ My first project, a Smarth Watch call Spectro.
+<br>
+In the future he will can do basically display `date` and `time`, set an `alarm`, ring a `buzzer` when the time comes, `temperature`, `barometer`, `altimeter`, `snooze` and also have `Bluetooth` to communication with phones.<br>
+For now we just have `date`, `time`, `temperature`, `barometer` and `altimeter. `<br>
+
+<br>
+Front <br>
+------------
+![project](http://oi66.tinypic.com/1212vj9.jpg)<br>
+
+Back <br>
+------------
+![project](http://i63.tinypic.com/1smutd.jpg)<br>
+<br>
+<br>
+Dependencies
+------------
+
+The code makes use of the ( for now)<br>
+[`sumotoy`](https://github.com/sumotoy/TFT_ILI9163C) - LCD<br>
+[`rodan`](https://github.com/rodan/ds3231) libraries. - RTC ( rename to DS3231 )<br>
+[`adafruit`](https://github.com/adafruit/Adafruit-GFX-Library) - complete ILI9163 Libraries<br>
+[`adafruit`](https://github.com/adafruit/Adafruit-BMP085-Library) - BMP180, <br>libraries.<br>
+
+Modules 
+------------
+
+Arduino Nano 3.0 <br>
+TFT Display 1,44" ILI9361 <br>
+DS3231 RTC <br>
+BMP180<br>
+HC-05 Bluetooth <br>
+4x Buttons<br>
+4x 1k Resistors  <br>
+2x 330 ohms Resistors <br>
+2x 680 ohms Resistors <br>
+
+<br>
+Wait for the How to.
---
+++ mainSpectro.ino
@@ -0,0 +1,132 @@
+#include <DS3231.h>
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <TFT_ILI9163C.h>
+#define  BLACK   0x0000
+#define  BLUE    0x001F
+#define  RED     0xF800
+#define  GREEN   0x07E0
+#define  CYAN    0x07FF
+#define  MAGENTA 0xF81F
+#define  YELLOW  0xFFE0  
+#define  WHITE   0xFFFF
+#define __CS 10
+#define __DC 8
+DS3231 Clock;
+bool Century=false;
+bool h12;
+bool PM;
+byte ADay, AHour, AMinute, ASecond, ABits;
+bool ADy, A12h, Apm;
+byte year, month, date, DoW, hour, minute, second, temperature;
+Adafruit_BMP085 bmp180;
+int i;
+TFT_ILI9163C tft = TFT_ILI9163C(__CS, __DC, 9);
+
+void setup() {
+  bmp180.begin();
+  tft.begin();
+  tft.fillScreen();
+  delay(500);
+  Wire.begin();
+  tft.setRotation(2);
+  Clock.setSecond(00);
+  Clock.setMinute(31);
+  Clock.setHour(15); 
+  Clock.setDoW(1); 
+  Clock.setDate(07); 
+  Clock.setMonth(02); 
+  Clock.setYear(16); 
+}
+
+void loop() {
+  relogioMenu1();
+  delay(60000);
+}
+
+void displayTime(){
+  
+ int second,minute,hour,date,month,year,temperature, day;
+ second=Clock.getSecond();
+ minute=Clock.getMinute();
+ hour=Clock.getHour(h12, PM);
+ date=Clock.getDate();
+ day=Clock.getDoW();
+ month=Clock.getMonth(Century);
+ year=Clock.getYear();
+  Serial.print(hour, DEC);
+  Serial.print(":");
+  if (minute<10)
+  {
+    Serial.print("0");
+  }
+  Serial.print(minute, DEC);
+  Serial.print(":");
+  if (second<10)
+  {
+    Serial.print("0");
+  }
+}
+
+void relogioMenu1(){
+  
+ int second,minute,hour,date,month,year,temperature;
+ second=Clock.getSecond();
+ minute=Clock.getMinute();
+ hour=Clock.getHour(h12, PM);
+ date=Clock.getDate();
+ month=Clock.getMonth(Century);
+ year=Clock.getYear();
+
+ temperature=Clock.getTemperature();
+ tft.setTextColor(WHITE);
+ tft.setTextSize(1);
+ tft.setCursor(40, 10);
+ tft.print(date,DEC);
+ tft.print('/');
+ tft.print(month,DEC);
+ tft.print('/');
+ tft.print("20");
+ tft.print(year,DEC);
+ tft.setCursor(5, 35);
+ tft.setTextColor(WHITE);
+ tft.setTextSize(4);
+ setWindowHoras();
+ tft.print(hour,DEC);
+ tft.print(':');
+ tft.print(minute,DEC);
+ setWindowTemp();
+ tft.setCursor(0, 107);
+ tft.setTextColor(WHITE);
+ tft.setTextSize(1);
+ tft.print("Temperatura:");
+ tft.setTextSize(2);
+ tft.setCursor(75, 100);
+ tft.setTextColor(RED);
+ tft.print(bmp180.readTemperature(),1);
+ tft.print('\n');
+ 
+}
+void setWindowSegundos(){
+for( i = 0; i < 1; i++)
+{
+tft.setAddrWindow(53, 64, 75, 86);
+tft.fillRect(53, 64, 22, 22, BLACK);
+}
+}
+void setWindowHoras(){
+for( i = 0; i < 1; i++)
+{
+tft.setAddrWindow(5, 35, 120, 75);
+tft.fillRect(5, 35, 120, 29, BLACK);
+}
+}
+void setWindowTemp(){
+for( i = 0; i < 1; i++)
+{
+tft.setAddrWindow(75, 90, 128, 128);
+tft.fillRect(74, 90, 500, 300, BLACK);
+}
+}
